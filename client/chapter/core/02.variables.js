ce311a2614aeb8b5af6439318dd26ad51c36647f
leesy <leesy787@gmail.com>
2023-07-04 16:04:02 +0900

    js 수업 2일차

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,74 @@
+/* --------------------------------------------------------------------- */
+/* Variables                                                             */
+/* --------------------------------------------------------------------- */
+// let admin;
+// let name;
+// name = 'john';
+// admin = name;
+// 윈도우가 가지고 있는 것이라 줄이 생긴거여
+// console.log(admin);
+
+
+// let planet = earth;
+// let username = 'john';
+
+// const ourPanetName = 'earth';
+// const currentUserName = 'john';
+
+// const admin; 은 에러 값을 할당 안 해줬기 때문
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+
+// - 갯수 별 상품 가격 계산하기
+ let calcProductPiceQuantity;
+
+ // - 구매 제품 가격의 총 합
+ let totalProductPrice;
+
+ // - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasClassName = false;
+// is (했어 안했어) has (있어 없어) 뭔가 여부를 물어보는 네이밍이다
+
+// - 구매 결제 내역
+ let paymentHistory; 
+
+ // - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday;
+
+// - 상품 정보
+const productInfomarion = '';
+// 이렇게 정해진 것은 아님
+
+/* variables ----------------------------------------------------------- */
+
+나의 작성
+// let price;
+// let paymentTotalSum;
+// const oneYearDayCount;
+// let PaymentStatus;
+// let PurchaseDetails;
+// const brandFirstName;
+// let todayDailyLuck;
+// const productInfo;
+
+
+
+/* constant variables -------------------------------------------------- */
+
+
+// panding 아무것도 안함
+// loading 로딩중 요청 대기
+// fulfilled, resolved 응답 성공
+// rejected 실패
+
+const PANDING = 'api/panding';
+const LOADING 
+// 끌어오자
--- client/main.js
+++ client/main.js
@@ -1,3 +1,2 @@
-console.log('hello.js');
-
-const a = 10;
+// console.log('hello.js');
+// const a = 10;
